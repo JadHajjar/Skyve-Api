b40a6be690158818828b24e1bfe82d400f2c1e2a
JadHajjar <jad@brains-lb.com>
2024-06-24 12:18:34 +0300

    Create RedirectController.cs

---
+++ Api/Controllers/RedirectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkyveApi.Controllers;
+public class RedirectController : ControllerBase
+{
+	[Route("/")]
+	public IActionResult Home()
+	{
+		return new RedirectResult("https://mods.paradoxplaza.com/mods/75804/Windows");
+	}
+
+	[Route("/discord")]
+	public IActionResult Discord()
+	{
+		return new RedirectResult("https://discord.gg/E4k8ZEtRxd");
+	}
+
+	[Route("/app/{*url}")]
+	public IActionResult App(string url)
+	{
+		return new RedirectResult($"skyve://{url}");
+	}
+}
