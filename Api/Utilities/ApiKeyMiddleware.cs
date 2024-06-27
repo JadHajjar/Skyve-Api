375b2ae3b5d7d49596a4f9a9b78cb97daf467425
JadHajjar <jad@brains-lb.com>
2024-06-27 18:19:19 +0300

    Update ApiKeyMiddleware.cs

--- Api/Utilities/ApiKeyMiddleware.cs
+++ Api/Utilities/ApiKeyMiddleware.cs
@@ -1,19 +1,25 @@
-﻿namespace SkyveApi.Utilities;
+﻿using System.Text.RegularExpressions;
+
+namespace SkyveApi.Utilities;
 
 public class ApiKeyMiddleware
 {
 	private readonly RequestDelegate _next;
 	private readonly string _apiKey;
+	private readonly Regex _regex;
 
 	public ApiKeyMiddleware(RequestDelegate next, string apiKey)
 	{
 		_next = next;
 		_apiKey = apiKey;
+		_regex = new Regex(@"^/\w+/api", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 	}
 
 	public async Task Invoke(HttpContext context)
 	{
-		if ((context.Request.Path.Value?.StartsWith("/api", StringComparison.InvariantCultureIgnoreCase) ?? true) &&
+		var regex = Regex.Match(context.Request.Path.Value ?? string.Empty, @"^/\w+/api", RegexOptions.IgnoreCase);
+
+		if (_regex.IsMatch(context.Request.Path.Value ?? string.Empty) &&
 			(!context.Request.Headers.TryGetValue("API_KEY", out var apiKeyValues)
 			|| apiKeyValues.Count == 0
 			|| apiKeyValues[0] != _apiKey))
