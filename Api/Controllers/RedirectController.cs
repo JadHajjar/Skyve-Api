9ce1b8343c6898775105b7b2cb761b7bb584065d
JadHajjar <jad@brains-lb.com>
2024-11-07 15:50:03 +0200

    Update RedirectController.cs

--- Api/Controllers/RedirectController.cs
+++ Api/Controllers/RedirectController.cs
@@ -15,6 +15,24 @@ public class RedirectController : ControllerBase
 		return new RedirectResult("https://discord.gg/E4k8ZEtRxd");
 	}
 
+	[Route("/guide")]
+	public IActionResult Guide()
+	{
+		return new RedirectResult("https://www.youtube.com/watch?v=mQvWrQ4rk_U");
+	}
+
+	[Route("/donate")]
+	public IActionResult Donate()
+	{
+		return new RedirectResult("https://ko-fi.com/chameleontbn");
+	}
+
+	[Route("/translate")]
+	public IActionResult Translate()
+	{
+		return new RedirectResult("https://crowdin.com/project/load-order-mod-2");
+	}
+
 	[Route("/app/{*url}")]
 	public IActionResult App(string url)
 	{
