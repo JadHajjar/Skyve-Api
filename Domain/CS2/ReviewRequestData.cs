292f422533649dd2b06aba86aae4678bf04e8e5d
JadHajjar <jad@brains-lb.com>
2025-04-24 15:49:18 +0300

    Update ReviewRequestData.cs

--- Domain/CS2/ReviewRequestData.cs
+++ Domain/CS2/ReviewRequestData.cs
@@ -18,29 +18,21 @@ public class ReviewRequestNoLogData : IDynamicSql
 	[DynamicSqlProperty(PrimaryKey = true)]
 	public string? UserId { get; set; }
 	[DynamicSqlProperty]
+	public bool IsMissingInfo { get; set; }
+	[DynamicSqlProperty]
 	public int PackageStability { get; set; }
 	[DynamicSqlProperty]
 	public int PackageUsage { get; set; }
 	[DynamicSqlProperty]
 	public int PackageType { get; set; }
 	[DynamicSqlProperty]
-	public int StatusType { get; set; }
-	[DynamicSqlProperty]
-	public int StatusAction { get; set; }
+	public int SavegameEffect { get; set; }
 	[DynamicSqlProperty]
 	public string? RequiredDLCs { get; set; }
-	[DynamicSqlProperty]
-	public string? StatusPackages { get; set; }
-	[DynamicSqlProperty]
-	public string? StatusNote { get; set; }
 	[DynamicSqlProperty]
 	public string? PackageNote { get; set; }
 	[DynamicSqlProperty]
 	public string? SaveUrl { get; set; }
-	[DynamicSqlProperty]
-	public bool IsInteraction { get; set; }
-	[DynamicSqlProperty]
-	public bool IsStatus { get; set; }
 	[DynamicSqlProperty]
 	public DateTime Timestamp { get; set; }
 }
