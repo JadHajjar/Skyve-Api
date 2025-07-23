a7ac6d26cdf1b9e52c5cda8ed0dd5fdbc231b9df
JadHajjar <jad@brains-lb.com>
2025-07-23 21:50:10 +0200

    Update RedirectLink.cs

--- Domain/Generic/RedirectLink.cs
+++ Domain/Generic/RedirectLink.cs
@@ -1,27 +1,21 @@
 ﻿using Extensions.Sql;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SkyveApi.Domain.Generic;
 [DynamicSqlClass("RedirectLinks")]
 public class RedirectLink : IDynamicSql
 {
-    [DynamicSqlProperty(PrimaryKey = true)]
-    public string? Key { get; set; }
-    [DynamicSqlProperty]
-    public string? Link { get; set; }
+	[DynamicSqlProperty(PrimaryKey = true)]
+	public string? Key { get; set; }
+	[DynamicSqlProperty]
+	public string? Link { get; set; }
 
 	public RedirectLink(string key)
 	{
 		Key = key;
 	}
 
-    public RedirectLink()
-    {
-        
-    }
+	public RedirectLink()
+	{
+
+	}
 }
