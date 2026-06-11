57a5abae87f46aaf6c571a95935d22c4a8a939a7
Jad Hajjar <jad.g.hajjar@hotmail.com>
2026-06-11 22:59:12 +0200

    Add obsolete page

---
+++ Api/Pages/Obsolete.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Extensions.Sql;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkyveApi.Pages;
+
+public class ObsoleteModel : PageModel
+{
+	[BindProperty(SupportsGet = true)]
+	public string? v { get; set; }
+
+	public void OnGet()
+	{
+	}
+}
